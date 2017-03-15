af8d013c2a6f12f0f7960d8bc4aa44af5010c1a2
eugenio <eugenio.606@gmail.com>
2017-03-15 04:24:44 -0700

    HOLA

--- src/ft_printf_o.c
+++ src/ft_printf_o.c
@@ -80,7 +80,9 @@ size_t	ft_printf_o_i(va_list ap, t_param *params)
 
 	length = 0;
 	n = va_arg(ap, unsigned int);
-	if (n == 0)
+	if (ft_printf_str_contains(params->flags, '#') && (params->specifier\
+		== 'o' || params->specifier == 'x' || params->specifier == 'X') &&\
+		length == 1 && *nbr == '0')//if (n == 0)
 		params->sign = -1;
 	nbr = ft_uitoa_base(n, 8);
 	length = ft_strlen(nbr);

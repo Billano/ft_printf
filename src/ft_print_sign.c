753b907cc1cf924990b4104248ad145aa4ea56e9
eugenio <eugenio.606@gmail.com>
2017-03-15 01:14:08 -0700

    HOLA

--- src/ft_print_sign.c
+++ src/ft_print_sign.c
@@ -20,7 +20,7 @@ void	ft_print_sign(t_param *params)
 			ft_putstr("0");
 		else if (params->specifier == 'x')
 			ft_putstr("0x");
-		else if (params->specifier == 'o')
+		else if (params->specifier == 'X')
 			ft_putstr("0X");
 	}
 	else if (params->sign == 0 && ft_printf_str_contains(params->flags, '+'))

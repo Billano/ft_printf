09a85d52c23299950c97ea9322a734c731fe36d3
eugenio <eugenio.606@gmail.com>
2017-03-18 02:02:46 -0700

    HOLA

--- src/ft_printf_c.c
+++ src/ft_printf_c.c
@@ -14,25 +14,25 @@
 
 void	ft_printf_print_lc(wchar_t n)
 {
-	if (chr <= 0x7F)
-		ft_putchar(chr);
-	else if (chr <= 0x7FF)
+	if (n <= 0x7F)
+		ft_putchar(n);
+	else if (n <= 0x7FF)
 	{
-		ft_putchar((chr >> 6) + 0xC0);
-		ft_putchar((chr & 0x3F) + 0x80);
+		ft_putchar((n >> 6) + 0xC0);
+		ft_putchar((n & 0x3F) + 0x80);
 	}
-	else if (chr <= 0xFFFF)
+	else if (n <= 0xFFFF)
 	{
-		ft_putchar((chr >> 12) + 0xE0);
-		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
-		ft_putchar((chr & 0x3F) + 0x80);
+		ft_putchar((n >> 12) + 0xE0);
+		ft_putchar(((n >> 6) & 0x3F) + 0x80);
+		ft_putchar((n & 0x3F) + 0x80);
 	}
-	else if (chr <= 0x10FFFF)
+	else if (n <= 0x10FFFF)
 	{
-		ft_putchar((chr >> 18) + 0xF0);
-		ft_putchar(((chr >> 12) & 0x3F) + 0x80);
-		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
-		ft_putchar((chr & 0x3F) + 0x80);
+		ft_putchar((n >> 18) + 0xF0);
+		ft_putchar(((n >> 12) & 0x3F) + 0x80);
+		ft_putchar(((n >> 6) & 0x3F) + 0x80);
+		ft_putchar((n & 0x3F) + 0x80);
 	}
 	// if (n <= 127)
 	// 	ft_putchar_n_bytes(n, 1);

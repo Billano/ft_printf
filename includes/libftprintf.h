8c4ae51cdab26eef9ab6dfa6f860d28e8ca844ee
eugenio <eugenio.606@gmail.com>
2017-03-18 02:09:27 -0700

    HOLA

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -65,7 +65,7 @@ void ft_printf_print_s(t_param *params, char *str);
 size_t ft_printf_s(va_list ap, t_param *params);
 
 // c, C
-void	ft_printf_print_lc(wchar_t n);
+size_t	ft_printf_print_lc(wchar_t n);
 size_t	ft_printf_lc(va_list ap, t_param *params);
 size_t	ft_printf_c(va_list ap, t_param *params);
 

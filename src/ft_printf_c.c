69dc94da63294593414591b4031bf6fcdab4f616
eugenio <eugenio.606@gmail.com>
2017-03-18 02:08:01 -0700

    HOLA

--- src/ft_printf_c.c
+++ src/ft_printf_c.c
@@ -14,52 +14,45 @@
 
 void	ft_printf_print_lc(wchar_t n)
 {
-	if (n <= 0x7F)
-		ft_putchar(n);
-	else if (n <= 0x7FF)
+	if (n <= 127)
 	{
-		ft_putchar((n >> 6) + 0xC0);
-		ft_putchar((n & 0x3F) + 0x80);
+		ft_putchar_n_bytes(n, 1);
+		return (1);
 	}
-	else if (n <= 0xFFFF)
+	else if (n <= 2047)
 	{
-		ft_putchar((n >> 12) + 0xE0);
-		ft_putchar(((n >> 6) & 0x3F) + 0x80);
-		ft_putchar((n & 0x3F) + 0x80);
+		ft_putchar_n_bytes(n, 2);
+		return (2);
 	}
-	else if (n <= 0x10FFFF)
+	else if (n <= 65535)
 	{
-		ft_putchar((n >> 18) + 0xF0);
-		ft_putchar(((n >> 12) & 0x3F) + 0x80);
-		ft_putchar(((n >> 6) & 0x3F) + 0x80);
-		ft_putchar((n & 0x3F) + 0x80);
+		ft_putchar_n_bytes(n, 3);
+		return (3);
 	}
-	// if (n <= 127)
-	// 	ft_putchar_n_bytes(n, 1);
-	// else if (n <= 2047)
-	// 	ft_putchar_n_bytes(n, 2);
-	// else if (n <= 65535)
-	// 	ft_putchar_n_bytes(n, 3);
-	// else if (n <= 1114111)
-	// 	ft_putchar_n_bytes(n, 4);
+	else if (n <= 1114111)
+	{
+		ft_putchar_n_bytes(n, 4);
+		return (4);
+	}
 }
 
 size_t	ft_printf_lc(va_list ap, t_param *params)
 {
 	wchar_t			n;
+	size_t length;
 
 	n = (wchar_t)va_arg(ap, wint_t);
 	if (ft_printf_str_contains(params->flags, '-'))
 	{
-		ft_printf_print_lc(n);
+		length = ft_printf_print_lc(n);
 		ft_print_blank_s(params, 1);
 	}
 	else
 	{
 		ft_print_blank_s(params, 1);
-		ft_printf_print_lc(n);
+		length = ft_printf_print_lc(n);
 	}
-	return (ft_max_number(params->width, 1));
+	return (ft_max_number(params->width, length));
 }
 
 size_t	ft_printf_c(va_list ap, t_param *params)
